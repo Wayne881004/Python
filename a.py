699426b481a9a26a7a786ab76a7cb6d5791e9373
Wayne881004 <t0919247347@gmail.com>
2021-02-25 23:56:56 +0800

    2021/2/25

--- a.py
+++ a.py
@@ -1,1 +1,10 @@
-print("aa")
+# ch1
+# name = input("what is your name? ")
+# color = input("What is your favorite color? ")
+# print(name + " likes " + color)
+
+#ch2
+# birthday = input("input your birth year ")
+# age = 2021 - int(birthday)
+# print("Your age is " + str(age))
+# print(type(birthday))
