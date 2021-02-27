defae0376664b8abb988f7628ea3a0bec04c3ec3
Wayne881004 <t0919247347@gmail.com>
2021-02-27 18:38:09 +0800

    2021/02/27

--- a.py
+++ a.py
@@ -8,3 +8,25 @@
 # age = 2021 - int(birthday)
 # print("Your age is " + str(age))
 # print(type(birthday))
+
+#ch3 String
+
+# print('""') ##want to print ""
+# course = 'Python for Beginners'
+# another = course[:]
+# print(course[-2])  
+# print(course[0:2])
+# print(course[2:-1])
+# print(another)
+# print(len(course))
+#i = 2 
+#print(course[0:i]) ##error
+
+# CV = '''
+# Hello,
+# my name is bobo.
+
+# '''
+# print(CV) ##want to send email
+
+
