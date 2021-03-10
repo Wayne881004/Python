217c8e94f6d166c949ce744c508062698b96ce32
Wayne881004 <t0919247347@gmail.com>
2021-03-10 22:08:02 +0800

    2021/03/10 string format

--- a.py
+++ a.py
@@ -8,3 +8,11 @@
 # age = 2021 - int(birthday)
 # print("Your age is " + str(age))
 # print(type(birthday))
+
+#ch3
+weight = int(input("Weight = "))
+unit = input("(L)bs  or (K)g:")
+if unit.upper() == 'L':
+    print("You are {}pounds.".format(weight * 0.45))
+else:
+    print("You are {} pounds.".format(weight / 0.45))
